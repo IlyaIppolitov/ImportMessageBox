c527fa0b0da219c84f0b98af9fb00c0ed5fa88c8
FinT08 <IppolitovIS@gmail.com>
2023-01-13 19:52:51 +0300

    Добавьте файлы проекта.

---
+++ ImportMessageBox/Program.cs
@@ -0,0 +1,17 @@
+﻿using ImportMessageBox;
+using System.Runtime.InteropServices;
+using System.Security.Cryptography.X509Certificates;
+using PInvoke;
+
+class Application
+{
+    public static void Main()
+    {
+        //Эта функция будет вызвана из DLL User32.dll
+        if (WinApiFunctions.MessageBox(0, "Выхотите полететь на Марс?", "Полёт",
+                                        WinApiFunctions.Constants.MB_YESNO | WinApiFunctions.Constants.MB_ICONQUESTION) == WinApiFunctions.Constants.IDYES)
+        {
+            WinApiFunctions.MessageBox(0, "Обратитесь к Илону!", "Полёт", 0);
+        }
+    }
+}
---
+++ ImportMessageBox/WinApiFunctions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+
+namespace ImportMessageBox
+{
+    internal static class WinApiFunctions
+    {
+        internal class Constants
+        {
+            public const uint MB_YESNO = (uint)0x00000004L;
+            public const uint MB_ICONQUESTION = (uint)0x00000020L;
+            public const int IDYES = 6;
+        }
+        
+        
+        //Опишем функцию, которую будем вызывать из DLL
+        [DllImport("user32.dll")]
+        public static extern int MessageBox(IntPtr hWnd, string message, string caption, uint decoration);
+
+    }
+}
